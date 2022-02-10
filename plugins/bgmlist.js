5a4badea3eccbf24012e0526d0b08eccd3fb9aec
ABU <98000024+Ajx-Abu@users.noreply.github.com>
2022-02-10 14:12:06 +0530

    Update bgmlist.js

--- plugins/bgmlist.js
+++ plugins/bgmlist.js
@@ -8,7 +8,7 @@ const config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'bgm list', fromMe: false, desc: 'send all bgm commands'}, (async (message, match) => {
+Asena.addCommand({pattern: 'bgm', fromMe: false, desc: 'send all bgm commands'}, (async (message, match) => {
 
     var r_text = new Array ();
     
