73dadf6602ac527a78519a902887f410057ed19d
ABU <98000024+Ajx-Abu@users.noreply.github.com>
2022-02-10 08:23:26 +0530

    ❤️ABU💙

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Miss Helena*:  ```" + msg + "```"
+    return "✅ *Abuser*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Miss Helena*:  ```" + msg + "```"
+    return "🛑 *Abuser*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Miss Helena*:  ```" + msg + "```"
+    return "⏺️ *Abuser*:  ```" + msg + "```"
 }
 
 
