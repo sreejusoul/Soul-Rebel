c1dcc31ba8884d574555b2057fbab6a57e27331d
ABU <98000024+Ajx-Abu@users.noreply.github.com>
2022-02-11 07:13:38 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,24 +7,24 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.ibb.co/mcVjXBN/IMG-20220120-WA0089.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/uucLoM6.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*✌︎➪𝑨𝑩𝑼 𝑺𝑬𝑹 𝑩𝑶𝑻☜︎︎︎✌︎*
-ᴄʀᴇᴀᴛᴏʀ ɴᴜᴍʙᴇʀ: wa.me/917025994178
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*💖𝐑𝐄𝐁𝐄𝐋 𝑺𝑬𝑹 𝑩𝑶𝑻☜︎︎︎✌︎*
+ᴄʀᴇᴀᴛᴏʀ ɴᴜᴍʙᴇʀ: wa.me/918547062392
 
-Tʜɪs ʙᴏᴛ Oᴡɴᴇʀ Nᴀᴍᴇ : ❤️🥰 ✌︎Aʙᴜ Sᴇʀ Bᴏᴛ ➪ Aʙᴜ»:-Jᴀsɪʟ ᴍᴜʜᴀᴍᴍᴇᴅ
+Tʜɪs ʙᴏᴛ Oᴡɴᴇʀ Nᴀᴍᴇ : ❤️🥰 ✌︎ʀᴇʙᴇʟ Sᴇʀ Bᴏᴛ ➪ ʀᴇʙᴇʟ»:-Jᴀsɪʟ ᴍᴜʜᴀᴍᴍᴇᴅ
 
-Aʙᴜ ʙᴏᴛ ɢʀᴏᴜᴘ: https://chat.whatsapp.com/Bq0eHs3UpGJ2BKIHOmy7mk
+ʀᴇʙᴇʟ ʙᴏᴛ ɢʀᴏᴜᴘ: https://chat.whatsapp.com/Bq0eHs3UpGJ2BKIHOmy7mk
 
-Gɪᴛʜᴜʙ ʟɪɴᴋ:    https://github.com/Ajx-Abu/Abu_ser
+Gɪᴛʜᴜʙ ʟɪɴᴋ:    https://github.com/Ajx-Abu/Soul-Rebel
 
-Aᴜᴅɪᴏ ᴄᴏᴍᴍᴀᴅᴇ :   https://github.com/Ajx-Abu/Abu_ser/tree/master/uploads
+Aᴜᴅɪᴏ ᴄᴏᴍᴍᴀᴅᴇ :   https://github.com/Ajx-Abu/Soul-Rebel/tree/master/uploads
 
-Sᴛɪᴄᴋᴇʀ ᴄᴏᴍᴍᴀᴅᴇ: https://github.com/Ajx-Abu/Abu_ser/tree/master/sticker
+Sᴛɪᴄᴋᴇʀ ᴄᴏᴍᴍᴀᴅᴇ: https://github.com/Ajx-Abu/Soul-Rebel/tree/master/sticker
 
 Cʜᴀɴɴᴇʟ : https://youtube.com/channel/UCWroqii8PORgNBdZO5MG6Fw 
 
-💝Aʙᴜ Sᴇʀ Bᴏᴛ Cʀᴇᴀᴛᴇᴅ Bʏ Aʙᴜ Sᴇʀ -Jᴀsɪʟ ᴍᴜʜᴀᴍᴍᴇᴅ💝
+💝ʀᴇʙᴇʟ Sᴇʀ Bᴏᴛ Cʀᴇᴀᴛᴇᴅ Bʏ ʀᴇʙᴇʟ Sᴇʀ 💝
 `}) 
 
 }));
